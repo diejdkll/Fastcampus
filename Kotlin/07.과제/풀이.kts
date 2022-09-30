43baebfc8fad7c755d1f081bd7f548ef35e414ad
hyeonseo0412 <s22026@gsm.hs.kr>
2022-09-30 22:46:41 +0900

    test

--- Kotlin/07.과제/풀이.kts
+++ Kotlin/07.과제/풀이.kts
@@ -1,22 +1,24 @@
-// 1.
+// 1번.
 var A: Int = 10
 var B: Int = 10
 var C: Boolean = if (A == B) true else false
 println(C)
 
-// 2.
+// 2번.
 var A2: Int = 10
 var B2: Int = A2 * 2
 println(B2)
 
-// 3.
+// 3번.
 fun testScore(score: Int): String {
+    //1번째 풀이
     // if
     if (score >= 90) return "A"
     else if (score >= 80) return "B"
     else if (score >= 70) return "C"
     else return "F"
 
+    //2번째 풀이
     // when
     when (score) {
         in 90..100 -> return "A"
@@ -26,12 +28,12 @@ fun testScore(score: Int): String {
     }
 }
 
-// 4.
+// 4번.
 fun testCount(A: Int): Int {
     return A * 5
 }
 
-// 5.
+// 5번.
 fun nullableSum(A: Int?, B: Int?): Int?{
     var AA: Int = if(A == null) 0 else A
     var BB: Int = if(B == null) 0 else B

ea501cc320e4c518b50362f3fdaee57558228dc4
hyeonseo0412 <s22026@gsm.hs.kr>
2022-10-06 20:44:39 +0900

    test

--- Kotlin/11.반복문/실습-while.kts
+++ Kotlin/11.반복문/실습-while.kts
@@ -1,11 +1,10 @@
 // While문
 var i = 0
-while(i < 5){
+while (i < 5) {
     println(i)
-    if(i%2 == 0){
+    if (i % 2 == 0) {
         println("짝수")
-    }
-    else{
+    } else {
         println("홀수")
     }
     i++ // 이부분을 제거하면 무한루프에 빠진다(루프(loop) : 반복)
@@ -13,7 +12,7 @@ while(i < 5){
 
 // Do/While문
 var z = 10
-do{
+do {
     println("시작")
     z++
-}while(z < 20)
+} while (z < 20)

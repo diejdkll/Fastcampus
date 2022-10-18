4fdffb46fb7429e4112fb0b57b974684e36e1f64
hyeonseo0412 <s22026@gsm.hs.kr>
2022-10-18 18:14:15 +0900

    test

--- Kotlin/16.클래스/실습-생성자.kts
+++ Kotlin/16.클래스/실습-생성자.kts
@@ -34,7 +34,8 @@ class User3(name: String) {
 }
 
 // 주생성자 -> 생략할 수 있는 모든걸 생략하는 방법
-class User4(val name: String)
+class User4(val name: String) {
+}
 
 // 주생성자 -> 기본값
 class User5(name: String = "김아무개") {
@@ -67,20 +68,53 @@ println(user6.age)
 // - 부생성자는 주생성자에게 생성을 위임해야 한다
 class User7 constructor(name: String) {
     var age: Int = 0
+    var nikename: String = ""
     val name: String
 
     init {
+        println("init")
         this.name = name
     }
 
     // 부생성자는 클래스명 우측에 올 수 없다 -> 클래스의 본문에 있어야 한다
-    constructor(name: String, age: Int) :this(name) {
+    constructor(name: String, age: Int) : this(name) {
+        println("second")
         this.age = age
+    }
+
+    // 부생성자는 여러개 존재할 수 있다
+    constructor(name: String, age: Int, nickname: String) : this(nickname) {
+
+    }
+
+}
+
+//val user7 = User7("가나다")
+//println(user7.name)
+val user7_2 = User7("가나다다", 50)
+println(user7_2.age)
+
+// 실행순서
+// 부생성자 호출 -> 부생성자 안에있는 주생성자 호출 -> init블록 호출 -> 부생성자 본문 실행
+// 클래스 속성에서 초기화를 시켜주지 않아도 되는 이유
+//  - 초기화 블록(init)에서 초기화를 보장해주기 때문 -> 클래스가 생성될 때 초기화 블록은 무조건 실행된다
+//  - 초기화 블록에(init) 없는 속성은 선언할 때 초기화 해줘야 한다
+
+class User8 {
+    val age: Int
+    val name: String
+
+    constructor(age: Int, name: String) {
+        this.age = age
+        this.name = name
     }
 }
 
-val user7 = User7("가나다")
-println(user7.name)
-val user7_2 = User7("가나다다", 50)
-println(user7_2.age)
+val user8 = User8(10, "가가가")
+println(user8.age)
+// 주생성자가 없는 것처럼 보이고 부생성자만 있을때 부생성자가 주생성자 처럼 보인다
+// -> 부생성자가 맞다!
+// -> 주생성자는 코틀린이 자동으로 만들어 준다
+// -> 주생성자가 없기 때문에 this()생성자를 이용해 생성을 위임할 필요가 없다
+
 
